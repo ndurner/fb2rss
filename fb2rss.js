3d3b3704aa34b9e2d818d55ab0f9b2bc29ce21fc
Nils Durner <ndurner@googlemail.com>
2018-04-01 21:39:55 +0200

    terminate process on errors

--- fb2rss.js
+++ fb2rss.js
@@ -48,200 +48,222 @@ function hash(s)
 
 async function writeRSSHeader(dst, page, doc, dt)
 {
-  var desc = await page.evaluate(() => {
-      return document.querySelector("meta[name='description']").getAttribute("content");
-    });  
-  
-  fs.writeSync(dst,
-    "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>" +
-    "<rss version=\"2.0\">" +
-    "<channel>" +
-    " <title>" + await page.title() + "</title>" +
-    " <description>" + desc + "</description>" +
-    " <link>" + page.url() + "</link>"
-  );
-
-  if (dt)
+  try {
+    var desc = await page.evaluate(() => {
+  return document.querySelector("meta[name='description']").getAttribute("content");
+      });  
+    
     fs.writeSync(dst,
-      " <lastBuildDate>" + dt.toUTCString() + "</lastBuildDate>" +
-      " <pubDate>" + dt.toUTCString() + "</pubDate>\n"
+      "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>" +
+      "<rss version=\"2.0\">" +
+      "<channel>" +
+      " <title>" + await page.title() + "</title>" +
+      " <description>" + desc + "</description>" +
+      " <link>" + page.url() + "</link>"
     );
+
+    if (dt)
+      fs.writeSync(dst,
+  " <lastBuildDate>" + dt.toUTCString() + "</lastBuildDate>" +
+  " <pubDate>" + dt.toUTCString() + "</pubDate>\n"
+      );
+  }
+  catch(error) {
+    console.error("writing RSS header failed: " + error);
+  }
 }
 
 function writeRSSFooter(dst)
 {
-  fs.writeSync(dst,
-    " </channel>" +
-    "</rss>"
-  );
+  try {
+    fs.writeSync(dst,
+      " </channel>" +
+      "</rss>"
+    );
+  }
+  catch(error) {
+    console.error("writing RSS footer failed: " + error);
+  }
 }
 
 function writeItem(dst, link, dt, title, content, guid)
 {
-  var dstr;
-  
-  if (dt)
-    dstr = " <pubDate>" + dt.toUTCString() + "</pubDate>";
-  else {
-    dstr = "";
-    console.log("no date: " + title);
+  try {
+    var dstr;
+    
+    if (dt)
+      dstr = " <pubDate>" + dt.toUTCString() + "</pubDate>";
+    else {
+      dstr = "";
+      console.log("no date: " + title);
+    }
+    
+    fs.writeSync(dst,
+      " <item>" +
+      " <title><![CDATA[" + title + "]]></title>" +
+      " <description><![CDATA[" + content + "]]></description>" +
+      " <link><![CDATA[" + link + "]]></link>" +
+      " <guid isPermaLink=\"false\"><![CDATA[" + guid + "]]></guid>" +
+      dstr +
+      " </item>\n"
+    );
   }
-  
-  fs.writeSync(dst,
-    " <item>" +
-    " <title><![CDATA[" + title + "]]></title>" +
-    " <description><![CDATA[" + content + "]]></description>" +
-    " <link><![CDATA[" + link + "]]></link>" +
-    " <guid isPermaLink=\"false\"><![CDATA[" + guid + "]]></guid>" +
-    dstr +
-    " </item>\n"
-  );
+  catch(error) {
+    console.error("writing RSS item failed: " + error);
+    process.exit(1);
+  }
 }
 
 async function saveRSS(url, destFN)
 {
-	const puppeteer = require('puppeteer');
-	const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  
-  await page.setViewport({width: 1920, height: 1050});
-  
-  console.log("opening " + url);
-  await page.goto(url, {waitUntil: 'domcontentloaded'});
-  await page.waitFor("//div[contains(@role, 'article') and not(contains(@class, 'UFIRow'))]");
-  
-	var baseURL = page.url();
-	baseURL = baseURL.substring(0, baseURL.lastIndexOf("/"));
-	
-	console.log("page opened");
+  try {
+    const puppeteer = require('puppeteer');
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    
+    await page.setViewport({width: 1920, height: 1050});
+    
+    console.log("opening " + url);
+    await page.goto(url, {waitUntil: 'domcontentloaded'});
+    await page.waitFor("//div[contains(@role, 'article') and not(contains(@class, 'UFIRow'))]");
+    
+    var baseURL = page.url();
+    baseURL = baseURL.substring(0, baseURL.lastIndexOf("/"));
+    
+    console.log("page opened");
 
-	const fb = await page.$('body');
+    const fb = await page.$('body');
 
-	const pageTitle = await page.title();
-	console.log("operating on page: " + pageTitle);
-	
-	if (pageTitle == '') {
-			console.log("empty page returned");
-			process.exit(1);
+    const pageTitle = await page.title();
+    console.log("operating on page: " + pageTitle);
+    
+    if (pageTitle == '') {
+	console.log("empty page returned");
+	process.exit(1);
+    }
+
+    var dst = fs.openSync(destFN, "w");
+    var name = pageTitle;
+    var articles = await page.$$("div[role *= 'article']:not(.UFIRow)");
+    var lastEntry;
+    
+    if (!articles) {
+      console.log("Could not load articles\n");
+      process.exit(1);
+    }
+
+    var date = await page.evaluate(() => {
+      return document.querySelector("abbr[data-utime]").getAttribute("data-utime");
+    });
+    if (date) {
+      lastEntry = new Date(1000 * date);
+    }
+    
+    await writeRSSHeader(dst, page, fb, lastEntry);
+
+    console.log("articles: " + articles.length);
+    for (var articleIdx = 0; articleIdx < articles.length; articleIdx++) {
+      var article = articles[articleIdx];
+      var title = undefined;
+      var content = undefined;
+      var dt = undefined;
+      var guid = undefined;
+
+      var articleUrl = await page.evaluate(article => {
+	return (article.getElementsByTagName("abbr")[0]).parentNode.getAttribute("href");
+	}, article);
+      var dto = await page.evaluate(article => {
+	return (article.getElementsByTagName("abbr")[0]).getAttribute("data-utime");
+	}, article);
+
+      if (articleUrl) {
+	if (articleUrl.substring(0, 1) == "/")
+	  articleUrl = baseURL + articleUrl;
+	  
+	guid = articleUrl;
+      }
+      else
+	articleUrl = url;
+
+      title = name;
+      
+      if (dto)
+	dt = new Date(1000 * dto);
+      else {
+	// look ahead to find an older article that does have a date set
+	// FIXME
+	/*
+	for (var olderIdx = articleIdx + 1; olderIdx < articles.length; olderIdx++) {
+	  dto = document.evaluate("string(//abbr[name(..) = 'a']/../@data-utime)", articles[olderIdx]).stringValue;
+	  if (dto)
+	    break;
 	}
-
-	var dst = fs.openSync(destFN, "w");
-	var name = pageTitle;
-	var articles = await page.$$("div[role *= 'article']:not(.UFIRow)");
-	var lastEntry;
+	* */
+	console.log("FIXME: NO DATE");
 	
-	if (!articles) {
-		console.log("Could not load articles\n");
-		process.exit(1);
-	}
+	if (dto)
+	  dt = new Date(1000 * dto);
+	else
+	  dt = lastEntry;
+      }
 
-	var date = await page.evaluate(() => {
-		return document.querySelector("abbr[data-utime]").getAttribute("data-utime");
-	});
-	if (date) {
-		lastEntry = new Date(1000 * date);
-	}
-	
-  await writeRSSHeader(dst, page, fb, lastEntry);
+      // remove user comments
+      await page.evaluate(article => {
+	var comments = article.querySelector("form");
+	if (comments)
+	  comments.parentNode.removeChild(comments);
+      }, article);
+	  
+      // extract content
+      var userContent = await page.evaluate(article => {
+	var userContent = article.querySelector("[class ~= 'userContent']");
+	if (userContent)
+	  userContent = userContent.querySelector("p");
+	  
+	return userContent ? userContent.innerText : "";      
+      }, article);
+      
+      content = await page.evaluate(article => {
+	return {
+	    text: article.innerText,
+	    html: article.innerHTML
+	  };
+      }, article);
+      
+      if (userContent)
+	title += ": " + userContent;
+      else
+	title = content.text;
+       
+      // use content hash key as GUID if nothing else unique is available
+      if (!guid)
+	guid = hash(baseURL + title + dt);
+      
+      writeItem(dst, articleUrl, dt, title, content.html, guid);
+    }
+    
+    writeRSSFooter(dst);
 
-	console.log("articles: " + articles.length);
-	for (var articleIdx = 0; articleIdx < articles.length; articleIdx++) {
-		var article = articles[articleIdx];
-		var title = undefined;
-		var content = undefined;
-		var dt = undefined;
-		var guid = undefined;
-
-		var articleUrl = await page.evaluate(article => {
-			return (article.getElementsByTagName("abbr")[0]).parentNode.getAttribute("href");
-			}, article);
-		var dto = await page.evaluate(article => {
-			return (article.getElementsByTagName("abbr")[0]).getAttribute("data-utime");
-			}, article);
-
-		if (articleUrl) {
-			if (articleUrl.substring(0, 1) == "/")
-				articleUrl = baseURL + articleUrl;
-				
-			guid = articleUrl;
-		}
-		else
-			articleUrl = url;
-
-		title = name;
-		
-		if (dto)
-			dt = new Date(1000 * dto);
-		else {
-			// look ahead to find an older article that does have a date set
-			// FIXME
-			/*
-			for (var olderIdx = articleIdx + 1; olderIdx < articles.length; olderIdx++) {
-				dto = document.evaluate("string(//abbr[name(..) = 'a']/../@data-utime)", articles[olderIdx]).stringValue;
-				if (dto)
-					break;
-			}
-			* */
-			console.log("FIXME: NO DATE");
-			
-			if (dto)
-				dt = new Date(1000 * dto);
-			else
-				dt = lastEntry;
-		}
-
-		// remove user comments
-		await page.evaluate(article => {
-			var comments = article.querySelector("form");
-			if (comments)
-				comments.parentNode.removeChild(comments);
-		}, article);
-				
-		// extract content
-		var userContent = await page.evaluate(article => {
-			var userContent = article.querySelector("[class ~= 'userContent']");
-			if (userContent)
-				userContent = userContent.querySelector("p");
-				
-			return userContent ? userContent.innerText : "";			
-		}, article);
-		
-		content = await page.evaluate(article => {
-			return {
-					text: article.innerText,
-					html: article.innerHTML
-				};
-		}, article);
-		
-		if (userContent)
-			title += ": " + userContent;
-		else
-			title = content.text;
-		 
-		// use content hash key as GUID if nothing else unique is available
-		if (!guid)
-			guid = hash(baseURL + title + dt);
-		
-		writeItem(dst, articleUrl, dt, title, content.html, guid);
-	}
-	
-	writeRSSFooter(dst);
-
-	fs.close(dst);
-	process.exit(0);
+    fs.close(dst);
+    process.exit(0);
+  }
+  catch(error) {
+    console.error("saveRSS() failed: " + error);
+    process.exit(1);
+  }
 }
 
 (async () => {
-	if (process.argv.length < 4) {
-		console.log("Usage: nodejs fb2rss.js url destination.rss");
-		process.exit(1);
-	}
+  if (process.argv.length < 4) {
+    console.log("Usage: nodejs fb2rss.js url destination.rss");
+    process.exit(1);
+  }
 
-	const puppeteer = require('puppeteer');
-	const browser = await puppeteer.launch();
+  const puppeteer = require('puppeteer');
+  const browser = await puppeteer.launch();
 
-	var url = process.argv[2];
-	var dest = process.argv[3];
+  var url = process.argv[2];
+  var dest = process.argv[3];
   
   return saveRSS(url, dest);
 })();
